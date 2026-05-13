2dfd37ba30877c4efa93f0893ecf7f4e037be56e
Ignacio Beccaria <ignabecca@gmail.com>
2026-05-13 19:55:49 -0300

    Nombre dias

--- src/COMPONENTES/WeatherCard.jsx
+++ src/COMPONENTES/WeatherCard.jsx
@@ -2,11 +2,21 @@ import React from "react"
 
 export default function WeatherCard({ fecha, max, min }) {
 
+  const fechaObjeto = new Date(fecha)
+
+  const nombreDia = fechaObjeto.toLocaleDateString("es-ES", {
+    weekday: "long"
+  })
+
+  const fechaFormateada = fechaObjeto.toLocaleDateString("es-AR")
+
   return (
 
     <div className="forecastCard">
 
-      <h3>{fecha}</h3>
+      <h2>{nombreDia}</h2>
+
+      <h3>{fechaFormateada}</h3>
 
       <p>🌡️ Max: {max}°C</p>
 
